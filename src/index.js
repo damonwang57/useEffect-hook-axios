ca13f611041c51af2139b44a6718a8a4dd1cb498
Damon <damon@e-diary.com>
2022-10-24 15:26:48 +1100

    Push Project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import AppFunctional from './AppFunctional';
+import './styles.css';
+
+ReactDOM.render(<AppFunctional />, document.getElementById('root'));
